8d3d20a1b22046051c3cf1b75447243c111dcf25
Clayton Franklin <clayton.j.franklin@nevada.unr.edu>
2019-04-27 19:31:47 -0700

    Fixing zsh install script

--- installShell.sh
+++ installShell.sh
@@ -18,3 +18,15 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # Install autojump
 apt install autojump
 
+# Change Shell to zsh
+chsh -s /bin/zsh
+
+# Ask user if they want to reboot
+read -p 'Do you want to restart now? [y/N]: ' response
+if ["$response" == "y"]; then
+    shutdown -r +1 "System will restart in one minute, please save all work."
+else
+    echo "zsh has been installed and is now the default shell.
+          The system will need to be rebooted in order to make the change permanent."
+fi
+
